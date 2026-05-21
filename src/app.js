22490d83ccdf8bbfde8c886b968b47d29e79a08a
Praveen <praveen.jaisingh@cloudrevelinnovation.com>
2026-05-21 10:17:39 +0530

    vercel issue fixed

--- src/app.js
+++ src/app.js
@@ -1,35 +1,57 @@
-
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
 const fs = require("fs");
-const resumeRoutes = require("./Routes/AiresumeanalyzerRoutes");
-const errorHandler = require("./Middleware/errorHendler");
+const resumeRoutes =require("./Routes/AiresumeanalyzerRoutes");
+const errorHandler =require("./Middleware/errorHendler");
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
+const uploadsDir =path.join("/tmp", "uploads");
 
-const uploadsDir = path.join(process.cwd(), "uploads");
-const generatedDir = path.join(process.cwd(), "generated");
+const generatedDir =path.join("/tmp", "generated");
 const initFolders = () => {
 
-    if (!fs.existsSync(uploadsDir)) {
+    try {
 
-        fs.mkdirSync(uploadsDir, {
-            recursive: true
-        });
+        if (
+            !fs.existsSync(uploadsDir)
+        ) {
+
+            fs.mkdirSync(
+                uploadsDir,
+                {
+                    recursive: true
+                }
+            );
+
+        }
+
+        if (
+            !fs.existsSync(generatedDir)
+        ) {
+
+            fs.mkdirSync(
+                generatedDir,
+                {
+                    recursive: true
+                }
+            );
+
+        }
+
+    } catch (error) {
+
+        console.error(
+            "Folder Init Error:",
+            error.message
+        );
 
     }
-    if (!fs.existsSync(generatedDir)) {
 
-        fs.mkdirSync(generatedDir, {
-            recursive: true
-        });
-
-    }
 };
 
 initFolders();
@@ -37,10 +59,12 @@ app.use(
     "/generated",
     express.static(generatedDir)
 );
-
 app.use(
     express.static(
-        path.join(process.cwd(), "public")
+        path.join(
+            process.cwd(),
+            "public"
+        )
     )
 );
 app.use(
@@ -50,14 +74,15 @@ app.use(
 app.get("/", (req, res) => {
 
     res.sendFile(
+
         path.join(
             process.cwd(),
             "public",
             "Airesumeanalyzer.html"
         )
+
     );
 
 });
 app.use(errorHandler);
-
 module.exports = app;
